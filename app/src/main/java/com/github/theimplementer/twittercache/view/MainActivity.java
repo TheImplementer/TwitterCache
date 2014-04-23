148eb0634e6eaf3d1c67ba40cf4b9e0b2307f10a
Enrico Pelizzon <enrico.pelizzon@gmail.com>
2014-04-24 00:00:02 +0100

    create activity to host the list of the tweets

---
+++ app/src/main/java/com/github/theimplementer/twittercache/view/MainActivity.java
@@ -0,0 +1,26 @@
+package com.github.theimplementer.twittercache.view;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+
+import com.github.theimplementer.twittercache.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final FragmentManager fragmentManager = getFragmentManager();
+        final Fragment container = fragmentManager.findFragmentById(R.id.tweet_list_container);
+        if (container == null) {
+            final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.tweet_list_container, TweetsListFragment.newInstance());
+            fragmentTransaction.commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/github/theimplementer/twittercache/view/TweetsListFragment.java
@@ -0,0 +1,28 @@
+package com.github.theimplementer.twittercache.view;
+
+
+import android.app.Fragment;
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+public class TweetsListFragment extends ListFragment {
+
+
+    public TweetsListFragment() {
+    }
+
+    public static Fragment newInstance() {
+        return new TweetsListFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
