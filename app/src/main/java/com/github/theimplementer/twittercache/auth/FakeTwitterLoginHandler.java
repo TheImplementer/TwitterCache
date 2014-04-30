09af0b1b63c8972a112716c95b4de8359263f780
Enrico Pelizzon <enrico.pelizzon@gmail.com>
2014-04-30 09:03:43 +0100

    added fake implementation of login handler

---
+++ app/src/main/java/com/github/theimplementer/twittercache/auth/FakeTwitterLoginHandler.java
@@ -0,0 +1,11 @@
+package com.github.theimplementer.twittercache.auth;
+
+import twitter4j.TwitterException;
+
+public class FakeTwitterLoginHandler implements TwitterLoginHandler {
+
+    @Override
+    public void login() throws TwitterException {
+
+    }
+}
