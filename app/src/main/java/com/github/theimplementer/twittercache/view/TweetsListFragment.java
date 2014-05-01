58b16767fe766140ef64baa51d3cf092c47ad99c
Enrico Pelizzon <enrico.pelizzon@gmail.com>
2014-05-02 00:38:31 +0100

    added logout button in options menu

--- app/src/main/java/com/github/theimplementer/twittercache/view/TweetsListFragment.java
+++ app/src/main/java/com/github/theimplementer/twittercache/view/TweetsListFragment.java
@@ -5,9 +5,14 @@ import android.app.Fragment;
 import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+
+import com.github.theimplementer.twittercache.R;
 
 public class TweetsListFragment extends ListFragment {
 
@@ -20,6 +25,12 @@ public class TweetsListFragment extends ListFragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1);
         setListAdapter(adapter);
@@ -29,5 +40,20 @@ public class TweetsListFragment extends ListFragment {
     @Override
     public void onStart() {
         super.onStart();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.main_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
